3fb71de01b6996e785a8d6529fa00f62cf79ddd2
brijeshbiet88@gmail.com <brijeshbiet88@gmail.com>
2021-05-21 19:03:01 +0530

    Added API to Get Tutorial By Id

--- src/main/java/com/cassandra/demo/controller/StudentController.java
+++ src/main/java/com/cassandra/demo/controller/StudentController.java
@@ -1,11 +1,13 @@
 package com.cassandra.demo.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,27 +15,38 @@ import org.springframework.web.bind.annotation.RestController;
 import com.cassandra.demo.model.Student;
 import com.cassandra.demo.repository.StudentRepository;
 
-import ch.qos.logback.classic.Logger;
 import lombok.extern.slf4j.Slf4j;
 
 @RestController
 @RequestMapping("/student")
 @Slf4j
 public class StudentController {
-	
+
 	@Autowired
-	StudentRepository studentRepository; 
+	StudentRepository studentRepository;
 
 	@GetMapping("/getall")
-	  public ResponseEntity<List<Student>> getAllStudents(@RequestParam(required = false) String title) {
-	    try {
-		  List<Student> students = studentRepository.findAll();
-		  return new ResponseEntity<List<Student>>(students , HttpStatus.OK);  
-	    }catch (Exception e) {
-	    	System.out.println(e.getMessage());
-	    	log.error("Error Occured :"+e.getMessage());
-		    return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
-		  }
-	  }
-	
+	public ResponseEntity<List<Student>> getAllStudents() {
+		try {
+			List<Student> students = studentRepository.findAll();
+			return new ResponseEntity<List<Student>>(students, HttpStatus.OK);
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+			log.error("Error Occured :" + e.getMessage());
+			return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+
+	@GetMapping("/getall/{registrationnumber}")
+	public ResponseEntity<Student> getAllStudentsWithId(@PathVariable String registrationnumber) {
+		try {
+			Optional<Student> students = studentRepository.findById(registrationnumber);
+			return new ResponseEntity<Student>(students.get(), HttpStatus.OK);
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+			log.error("Error Occured :" + e.getMessage());
+			return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+
 }
--- src/main/java/com/cassandra/demo/controller/TutorialController.java
+++ src/main/java/com/cassandra/demo/controller/TutorialController.java
@@ -1,11 +1,14 @@
 package com.cassandra.demo.controller;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,10 +27,20 @@ public class TutorialController {
   TutorialRepository tutorialRepository;
 
   @GetMapping("/tutorials")
-  public ResponseEntity<List<Tutorial>> getAllTutorials(@RequestParam(required = false) String title) {
+  public ResponseEntity<List<Tutorial>> getAllTutorials() {
     try {
 	  List<Tutorial> tutorials = tutorialRepository.findAll();
 	  return new ResponseEntity<List<Tutorial>>(tutorials , HttpStatus.OK);  
+    }catch (Exception e) {
+	    return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+	  }
+  }
+  
+  @GetMapping("/tutorials/{id}")
+  public ResponseEntity<Tutorial> getTutorialById(@PathVariable UUID id) {
+    try {
+	  Optional<Tutorial> tutorials = tutorialRepository.findById(id);
+	  return new ResponseEntity<Tutorial>(tutorials.get() , HttpStatus.OK);  
     }catch (Exception e) {
 	    return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
 	  }
@@ -36,7 +49,7 @@ public class TutorialController {
   @PostMapping("/tutorials")
   public ResponseEntity<Tutorial> createTutorial(@RequestBody Tutorial tutorial) {
 	  try {
-		    Tutorial _tutorial = tutorialRepository.save(new Tutorial(UUIDs.timeBased(), tutorial.getTitle(), tutorial.getDescription(), false));
+		    Tutorial _tutorial = tutorialRepository.save(new Tutorial(UUIDs.timeBased(), tutorial.getTitle(), tutorial.getDescription(), tutorial.isPublished()));
 		    return new ResponseEntity<>(_tutorial, HttpStatus.CREATED);
 		  } catch (Exception e) {
 		    return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
